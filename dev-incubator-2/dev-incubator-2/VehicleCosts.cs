648bda1bd486435421688a10393ff5df1dea2337
Leonid Boginski <leonid.boginski@gmail.com>
2020-09-17 01:16:42 +0300

    Feat add class VehicleCosts

---
+++ dev-incubator-2/dev-incubator-2/VehicleCosts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_incubator_2
+{
+    public class VehicleCosts
+    {
+        public string name;
+        public double averageCostKilometer;     //BYN (cost - 1 km)
+
+        public VehicleCosts() { }
+
+        public VehicleCosts(string name, double averageCostKilometer)
+        {
+            this.name = name;
+            this.averageCostKilometer = averageCostKilometer;
+        }
+    }
+}
