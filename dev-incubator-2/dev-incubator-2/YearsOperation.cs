ebef18a397f40e2e89d1f0565551613b61a1f071
Leonid Boginski <leonid.boginski@gmail.com>
2020-09-16 16:27:37 +0300

    Feat add enum years operation

---
+++ dev-incubator-2/dev-incubator-2/YearsOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_incubator_2
+{
+    public enum YearsOperation
+    {
+        Y2010,
+        Y2011,
+        Y2012,
+        Y2013,
+        Y2014,
+        Y2015,
+        Y2016,
+        Y2017,
+        Y2018,
+        Y2019,
+        Y2020
+    }
+}
