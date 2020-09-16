5ca539beddd9d3e19c21aac463407b6e8c6a723c
Leonid Boginski <leonid.boginski@gmail.com>
2020-09-16 16:47:01 +0300

    Feat add class Vehicle

---
+++ dev-incubator-2/dev-incubator-2/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_incubator_2
+{
+    public class Vehicle : IComparable
+    {
+        public string name { get; set; }
+        public double averageCost { get; set; }                 //BYN (cost - 1 km)
+        public double averageKilometers { get; set; }           //Km (kilometers - 1 month)
+        public YearsOperation startOperation { get; set; }      //Year (from enum)
+
+        public Vehicle() { }
+
+        public Vehicle(string name, double averageCost, double averageKilometers, YearsOperation startOperation) 
+        {
+            this.name = name;
+            this.averageCost = averageCost;
+            this.averageKilometers = averageKilometers;
+            this.startOperation = startOperation;
+        }
+
+        public double GetTotalCosts() => averageCost * averageKilometers;
+
+        public override string ToString() => "{name},{averageCost},{averageKilometers},{startOperation}";
+
+        public int CompareTo(object obj) 
+        {
+            Vehicle v = (Vehicle)obj;
+            if (GetTotalCosts() < v.GetTotalCosts()) return -1;
+            else if (GetTotalCosts() > v.GetTotalCosts()) return 1;
+            else return 0;
+        }
+    }
+}
