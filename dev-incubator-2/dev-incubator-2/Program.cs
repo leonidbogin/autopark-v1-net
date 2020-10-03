026abcba65de8b9b9dd63d89c8546ca78cc87329
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-03 13:34:55 +0300

    Refactor (Task2)

--- dev-incubator-2/dev-incubator-2/Program.cs
+++ dev-incubator-2/dev-incubator-2/Program.cs
@@ -12,12 +12,14 @@ namespace dev_incubator_2
         {
             const int n = 5;
 
-            Vehicle[] vehicles = new Vehicle[n];
-            vehicles[0] = new Vehicle("МАЗ 5551", 4.5, 300, YearsOperation.Y2018);
-            vehicles[1] = new Vehicle("МАЗ 5516", 4, 400, YearsOperation.Y2019);
-            vehicles[2] = new Vehicle("Volkswagen Passat B5", 0.6, 800, YearsOperation.Operation);
-            vehicles[3] = new Vehicle("Volkswagen Polo", 0.4, 400, YearsOperation.Y2020);
-            vehicles[4] = new Vehicle("МАЗ 216", 2.8, 1200, YearsOperation.Operation);
+            Vehicle[] vehicles = new Vehicle[n]
+            {
+                new Vehicle("МАЗ 5551", 4.5, 300, YearsOperation.Y2018),
+                new Vehicle("МАЗ 5516", 4, 400, YearsOperation.Y2019),
+                new Vehicle("Volkswagen Passat B5", 0.6, 800, YearsOperation.Operation),
+                new Vehicle("Volkswagen Polo", 0.4, 400, YearsOperation.Y2020),
+                new Vehicle("МАЗ 216", 2.8, 1200, YearsOperation.Operation)
+            };
 
             PrintVehicle(vehicles);
 
@@ -25,18 +27,18 @@ namespace dev_incubator_2
             foreach (Vehicle vehicle in vehicles)
             {
                 if (vehicle != null)
-                    averageCost += vehicle.averageCostKilometer;
+                    averageCost += vehicle.AverageCostKilometer;
             }
-            averageCost = averageCost / n;
+            averageCost /= n;
             Console.WriteLine($"\nAverage cost = {averageCost}");
 
             int indexMax = -1;
             for (int i = 0; i < n; i++)
-                if (vehicles[i].endOperation != YearsOperation.Operation)
-                    if (indexMax < 0 || vehicles[indexMax].averageCostKilometer < vehicles[i].averageCostKilometer)
+                if (vehicles[i].EndOperation != YearsOperation.Operation)
+                    if (indexMax < 0 || vehicles[indexMax].AverageCostKilometer < vehicles[i].AverageCostKilometer)
                         indexMax = i;
             if (indexMax > -1) 
-                Console.WriteLine($"Decommissioning year with maximum cost = {(int)vehicles[indexMax].endOperation}\n");
+                Console.WriteLine($"Decommissioning year with maximum cost = {(int)vehicles[indexMax].EndOperation}\n");
             else
                 Console.WriteLine($"Decommissioning year with maximum cost not found\n");       
 
--- dev-incubator-2/dev-incubator-2/Vehicle.cs
+++ dev-incubator-2/dev-incubator-2/Vehicle.cs
@@ -8,24 +8,24 @@ namespace dev_incubator_2
 {
     public class Vehicle : IComparable
     {
-        public string name { get; set; }
-        public double averageCostKilometer { get; set; }        //BYN (cost - 1 km)
-        public double averageKilometers { get; set; }           //Km (kilometers - 1 month)
-        public YearsOperation endOperation { get; set; }        //Year (from enum)
+        public string Name { get; set; }
+        public double AverageCostKilometer { get; set; }        //BYN (cost - 1 km)
+        public double AverageKilometers { get; set; }           //Km (kilometers - 1 month)
+        public YearsOperation EndOperation { get; set; }        //Year (from enum)
 
         public Vehicle() { }
 
         public Vehicle(string name, double averageCostKilometer, double averageKilometers, YearsOperation endOperation) 
         {
-            this.name = name;
-            this.averageCostKilometer = averageCostKilometer;
-            this.averageKilometers = averageKilometers;
-            this.endOperation = endOperation;
+            this.Name = name;
+            this.AverageCostKilometer = averageCostKilometer;
+            this.AverageKilometers = averageKilometers;
+            this.EndOperation = endOperation;
         }
 
-        public double GetTotalCosts() => averageCostKilometer * averageKilometers;
+        public double GetTotalCosts() => AverageCostKilometer * AverageKilometers;
 
-        public override string ToString() => $"{name};{averageCostKilometer};{averageKilometers};{endOperation}"
+        public override string ToString() => $"{Name};{AverageCostKilometer};{AverageKilometers};{EndOperation}"
                                 .Replace(',', '.').Replace(';', ','); //Fix replacing ',' in numbers with '.'
 
         public int CompareTo(object obj) 
